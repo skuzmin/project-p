b8dcac0e22638f0cb437b2207a1749512fa1dd99
skuzmin <erngorn@gmail.com>
2020-04-02 13:08:44 +0300

    added evn variables

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,5 @@
 import * as axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://3.15.208.153:9001/api/',
-    // baseURL: 'http://localhost:9001/api',
+    baseURL: process.env.VUE_APP_API_URL,
 });
